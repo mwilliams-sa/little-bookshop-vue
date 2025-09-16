540e8b9f2521bde2f8d0c90d85dff9268edf6487
mwilliams-sa <mwilliams@sygnia.co.za>
2025-09-16 12:34:25 +0200

    home.js

--- home.js
+++ home.js
@@ -1,1 +1,8 @@
-export default { template: `<section class="page"><h2>Home</h2><p>…</p></section>` };
+export default {
+  template: `
+    <section class="page">
+      <h2>Welcome to the Little Bookshop</h2>
+      <p>Browse our shelves and discover your next favourite read.</p>
+    </section>
+  `,
+};
