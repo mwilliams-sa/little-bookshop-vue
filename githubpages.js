e8814473c0cf504e21930c709a86ecac9315d159
mwilliams-sa <mwilliams@sygnia.co.za>
2025-09-16 09:22:41 +0200

    githubpages.js

---
+++ githubpages.js
@@ -0,0 +1,13 @@
+ Octokit.js
+ httpsgithub.comoctokitcore.js#readme
+const octokit = new Octokit({
+  auth 'YOUR-TOKEN'
+})
+
+await octokit.request('GET repos{owner}{repo}pages', {
+  owner 'OWNER',
+  repo 'REPO',
+  headers {
+    'X-GitHub-Api-Version' '2022-11-28'
+  }
+})
